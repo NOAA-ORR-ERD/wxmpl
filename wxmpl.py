22aeb5d31862263e46561b18d33e30834d48873e
Roland Arsenault <roland@ccom.unh.edu>
2022-07-20 09:25:18 -0400

    Fix except statement.

--- wxmpl.py
+++ wxmpl.py
@@ -1504,7 +1504,7 @@ class PlotFrame(wx.Frame):
 
         try:
             self.panel.print_figure(fileName)
-        except IOError, e:
+        except IOError as e:
             if e.strerror:
                 err = e.strerror
             else:
